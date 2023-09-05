314fdefd9ddd40b4a9ff4f522e29f6684ca89851
moshiur25524 <moshiur.rahman25524@gmail.com>
2023-09-05 13:36:09 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Header from "./pages/Header/Header";
+import Posts from "./pages/Posts/Posts";
+function App() {
+  return (
+    <main>
+      <Header />
+      <Posts />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/pages/Posts/Posts.jsx
@@ -0,0 +1,52 @@
+import "./Posts.css";
+
+const Posts = () => {
+  return (
+    <>
+      <div className="post">
+        <img
+          src="https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8dGVjaG5vbG9neXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60"
+          alt=""
+        />
+        <div className="texts">
+          <h2>Full-house battery backup coming later this year</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio,
+            inventore? Iusto cumque totam expedita quasi vitae voluptate
+            deserunt, quo mollitia?
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <img
+          src="https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8dGVjaG5vbG9neXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60"
+          alt=""
+        />
+        <div className="texts">
+          <h2>Full-house battery backup coming later this year</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio,
+            inventore? Iusto cumque totam expedita quasi vitae voluptate
+            deserunt, quo mollitia?
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <img
+          src="https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8dGVjaG5vbG9neXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60"
+          alt=""
+        />
+        <div className="texts">
+          <h2>Full-house battery backup coming later this year</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio,
+            inventore? Iusto cumque totam expedita quasi vitae voluptate
+            deserunt, quo mollitia?
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Posts;
