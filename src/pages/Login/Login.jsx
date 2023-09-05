ab111dcbc35e3cd2e76f8de4daf0b224bf1bf375
moshiur25524 <moshiur.rahman25524@gmail.com>
2023-09-05 19:07:11 +0600

    worked on login and register page and created a server

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,23 +1,28 @@
+import { useState } from "react";
 import "./Login.css";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log(username, password);
+    e.target.reset();
+  };
   return (
     <div>
-      <form action="" className="login">
+      <form action="" className="login" onSubmit={handleLogin}>
         <h1>Login</h1>
         <input
-          type="email"
-          name="email"
-          placeholder="Your email"
-          id=""
-          required
+          type="text"
+          placeholder="userName"
+          onChange={(e) => setUsername(e.target.value)}
         />
         <input
           type="password"
-          name="password"
+          onChange={(e) => setPassword(e.target.value)}
           placeholder="Your password"
-          id=""
-          required
         />
         <button>Login</button>
       </form>
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,21 +1,28 @@
+import { useState } from "react";
+
 const Register = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    console.log(username, password);
+    e.target.reset();
+  };
   return (
     <>
-      <form action="" className="register">
+      <form action="" className="register" onSubmit={handleRegister}>
         <h1>Register</h1>
         <input
-          type="email"
-          name="email"
-          placeholder="Your email"
-          id=""
-          required
+          type="text"
+          placeholder="username"
+          onChange={(e) => setUsername(e.target.value)}
         />
         <input
           type="password"
           name="password"
           placeholder="Your password"
-          id=""
-          required
+          onChange={(e) => setPassword(e.target.value)}
         />
         <button>Register</button>
       </form>
